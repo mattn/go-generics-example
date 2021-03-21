3e1d0c700207d0664e32be4500221d0b1d30e030
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-21 23:29:36 +0900

    Add add

---
+++ add/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Addable interface {
+	type int, int8, int16, int32, int64,
+		uint, uint8, uint16, uint32, uint64, uintptr,
+		float32, float64, complex64, complex128,
+		string
+}
+
+func add[T Addable](a, b T) T {
+    return a + b
+}
+
+func main() {
+    fmt.Println(add(1,2))
+
+    // FIXME
+    //fmt.Println(add("foo","bar"))
+}
+
