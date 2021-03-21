98cc063bf09162e43661bb918d573a91e302885c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-21 22:34:27 +0900

    Add ternalyOp

--- main.go
+++ main.go
@@ -22,16 +22,27 @@ func mapFunc[T any, M any](a []T, f func(T) M) []M {
     return n
 }
 
-func main() {
-    vi := []int{1,2,3,4,5,6}
+func ternaryOp[T any](s bool, t T, f T) T {
+    if s {
+        return t
+    }
+    return f
+}
 
+func main() {
+    // using filterFunc
+    vi := []int{1,2,3,4,5,6}
     vi = filterFunc(vi, func(v int) bool {
         return v < 4
     })
     fmt.Println(vi)
 
+    // using mapFunc
     vs := mapFunc(vi, func(v int) string {
         return "<" + fmt.Sprint(v) + ">"
     })
     fmt.Println(vs)
+
+    // using ternaryOp
+    fmt.Println(ternaryOp(len(vs) < 5, "short", "long"))
 }
