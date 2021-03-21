a95ade3d6acd9ff25c0d3f53d2822e654558c55d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-21 23:57:27 +0900

    Add pointer

---
+++ pointer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+)
+
+func pointerOf[T any](v T) *T {
+    return &v
+}
+
+func main() {
+    sp := pointerOf("foo")
+    fmt.Println(*sp)
+
+    ip := pointerOf(123)
+    fmt.Println(*ip)
+    *ip = 234
+    fmt.Println(*ip)
+}
+
