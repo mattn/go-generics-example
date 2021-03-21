4003e2335c5f8a68f645e548cff41c8c06006370
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-03-22 00:19:23 +0900

    Add splittraintest

---
+++ splittraintest/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func shuffle[T any](a []T) {
+    n := len(a)
+    for i := n - 1; i >= 0; i-- {
+        j := rand.Intn(i + 1)
+        a[i], a[j] = a[j], a[i]
+    }
+}
+
+func splitTrainTest[T any](a []T, f float64) ([]T, []T) {
+    aa := make([]T, len(a), len(a))
+    copy(aa, a)
+    shuffle(aa)
+    i := int(float64(len(aa)) * f)
+    return aa[:i], aa[i:]
+}
+
+func main() {
+    vi := []int{1,2,3,4,5,6,7,8,9,10}
+    train, test := splitTrainTest(vi, 0.75)
+    fmt.Println(train, test)
+}
+
