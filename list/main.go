0b1c5b213090524057fdf2273066aa5f87c45f1b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-09-21 14:43:12 +0900

    go fmt

--- list/main.go
+++ list/main.go
@@ -13,35 +13,35 @@ func (l *List[T]) Push(v T) {
 }
 
 func (l *List[T]) Insert(v T) {
-    l.l, l.l[0] = append(l.l[:1], l.l[0:]...), v
-    return
+	l.l, l.l[0] = append(l.l[:1], l.l[0:]...), v
+	return
 }
 
 func (l *List[T]) InsertAt(pos int, v T) {
-    l.l, l.l[0] = append(l.l[:pos+1], l.l[pos:]...), v
-    return
+	l.l, l.l[0] = append(l.l[:pos+1], l.l[pos:]...), v
+	return
 }
 
 func (l *List[T]) Remove(i int) {
-    l.l = l.l[:i+copy(l.l[i:], l.l[i+1:])]
-    return
+	l.l = l.l[:i+copy(l.l[i:], l.l[i+1:])]
+	return
 }
 
 func (l *List[T]) Equals(rhs *List[T]) bool {
-    if len(l.l) != len(rhs.l) {
-        return false
-    }
-    for i := 0; i < len(l.l); i++ {
-        if l.l[i] != rhs.l[i] {
-            return false
-        }
-    }
-    return true
+	if len(l.l) != len(rhs.l) {
+		return false
+	}
+	for i := 0; i < len(l.l); i++ {
+		if l.l[i] != rhs.l[i] {
+			return false
+		}
+	}
+	return true
 }
 
 func (l *List[T]) Clone() *List[T] {
-    ll := &List[T]{l: make([]T, len(l.l))}
-    copy(ll.l, l.l)
+	ll := &List[T]{l: make([]T, len(l.l))}
+	copy(ll.l, l.l)
 	return ll
 }
 
@@ -59,9 +59,8 @@ func main() {
 	l.Push(5)
 	l.Push(6)
 	fmt.Println(l.Slice())
-    l.Remove(3)
+	l.Remove(3)
 	fmt.Println(l.Slice())
-    c := l.Clone()
-    fmt.Println(c.Equals(l))
+	c := l.Clone()
+	fmt.Println(c.Equals(l))
 }
-
