4548d9ce0d0e5c49c76a200b7a7d98ccfe264014
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-09-25 14:35:38 +0900

    Add constraints-vector

---
+++ constraints-vector/go.mod
@@ -0,0 +1,3 @@
+module github.com/mattn/go-generics-example/constraints-vector
+
+go 1.18
---
+++ constraints-vector/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"constraints"
+	"fmt"
+)
+
+type v[T constraints.Ordered] T
+
+type Vector[T constraints.Ordered] struct {
+	x, y T
+}
+
+func (v *Vector[T]) Add(x, y T) {
+	v.x += T(x)
+	v.y += T(y)
+}
+
+func (v *Vector[T]) String() string {
+	return fmt.Sprintf("{x: %v, y: %v}", v.x, v.y)
+}
+
+func NewVector[T constraints.Ordered](x, y T) *Vector[T] {
+	return &Vector[T]{x: x, y: y}
+}
+
+func main() {
+	v := NewVector[float64](1, 2)
+	v.Add(2, 3)
+	fmt.Println(v)
+}
