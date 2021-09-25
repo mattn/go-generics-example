188fa6497c73f66a55ef066d4db677d1aa64abef
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-09-25 11:05:36 +0900

    go fmt

--- constraints-chan/main.go
+++ constraints-chan/main.go
@@ -1,32 +1,29 @@
-// NOTE This won't work yet
-// https://github.com/golang/go/issues/48609
-
 package main
 
 import (
-    "constraints"
-    "context"
-    "fmt"
+	"constraints"
+	"context"
+	"fmt"
 )
 
 func makeChan[T constraints.Chan[E], E any](ctx context.Context, arr []E) T {
-    ch := make(T)
-    go func() {
-        defer close(ch)
-        for _, v := range arr {
-            select {
-            case <-ctx.Done():
-                return
-            default:
-            }
-            ch <- v
-        }
-    }()
-    return ch
+	ch := make(T)
+	go func() {
+		defer close(ch)
+		for _, v := range arr {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			ch <- v
+		}
+	}()
+	return ch
 }
 
 func main() {
-    for v := range makeChan(context.Background(), []int{1, 2, 3}) {
-        fmt.Println(v)
-    }
+	for v := range makeChan(context.Background(), []int{1, 2, 3}) {
+		fmt.Println(v)
+	}
 }
