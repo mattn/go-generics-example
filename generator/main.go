215d85e65b2f0817a91e92182ee972a55803fc61
Colin King <me@colinking.co>
2021-10-10 18:21:51 -0400

    Update to use type sets

--- generator/main.go
+++ generator/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Addable interface {
-	type int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr, float32, float64, complex64, complex128
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | complex64 | complex128
 }
 
 func generator[T Addable](a T, v T) func() T {
--- minmax/main.go
+++ minmax/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 type comparable interface {
-	type int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64, uintptr, float32, float64
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64
 }
 
 func max[T comparable](a []T) T {
