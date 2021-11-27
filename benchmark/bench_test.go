8bf878e9c532717e5761f765b512425f8c965737
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2021-11-28 00:38:22 +0900

    Add benchmark

---
+++ benchmark/bench_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+)
+
+func addInt(a, b int) int {
+	return a + b
+}
+
+func addString(a, b string) string {
+	return a + b
+}
+
+func BenchmarkWithoutGenerics(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = addInt(1, 2)
+		_ = addString("foo", "bar")
+	}
+}
+
+type Addable interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | uintptr | float32 | float64 | complex64 | complex128 | string
+}
+
+func add[T Addable](a, b T) T {
+	return a + b
+}
+
+func BenchmarkWithGenerics(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = add(1, 2)
+		_ = add("foo", "bar")
+	}
+}
---
+++ benchmark/go.mod
@@ -0,0 +1,3 @@
+module github.com/mattn/go-generics-example/benchmark
+
+go 1.18
